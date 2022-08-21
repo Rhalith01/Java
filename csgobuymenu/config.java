9a4343332a4b7dba8da2fa4fd95f1fc10abb219b
Yavuz Selimhan KÖROĞLU <86046468+Rhalith01@users.noreply.github.com>
2022-08-21 23:15:47 +0300

    Add files via upload

---
+++ csgobuymenu/config.java
@@ -0,0 +1,15 @@
+
+public class config {
+	String weapon_usp = "USP ", weapon_tec9 = "TEC9 ", weapon_p250 = "P250 ", weapon_deagle = "Desert Eagle ",
+			weapon_fiveseven = "FIVESEVEN ", weapon_m4a1 = "M4A1 ", weapon_ak47 = "AK47 ", weapon_aug = "AUG ",
+			weapon_awp = "AWP ", weapon_bizon = "BIZON ", weapon_famas = "FAMAS ", weapon_gs3sg1 = "GS3SG1 ",
+			weapon_galilar = "GALIL ", weapon_m249 = "M249 ", weapon_mac10 = "MAC10 ", weapon_p90 = "P90",
+			weapon_decoygrenade = "Decoy Grenade" , weapon_explosive = "High Explosive Grenade", weapon_mag7 = "MAG7 ", weapon_mp7 = "MP7 ", weapon_mp9 = "MP9 ",
+			weapon_negev = "NEGEV ", weapon_nova = "NOVA ", weapon_scar20 = "SCAR20 ", weapon_sg556 = "SG556 ",
+			weapon_ssg08 = "SSG08 ", weapon_ump45 = "UMP45 ", weapon_xm1014 = "XM1014 ", weapon_elite = "ELITE ",
+			weapon_glock = "GLOCK ", weapon_hegranede = "HEGRANEDE ", weapon_flashbang = "FLASHBANG ",
+			weapon_incgrenade = "INCGRENADE" , weapon_smoke = "Smoke Grenade", weapon_molotov = "MOTOLOV", inventory = "",
+			weapon_kevlarvest = "Kevlar Vest ", weapon_zeus = "ZEUS X27 ", weapon_defuse = "Defuse Kit ",
+			weapon_kevlarhelmet = "Kevlar + Helmet ";
+
+}
